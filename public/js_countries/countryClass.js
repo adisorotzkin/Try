ea59bc8e5de1d19bf75abfc61b7e20495d3be659
adisorotzkin <adisorotz@gmail.com>
2023-11-29 10:47:23 +0200

    changedUrl

--- public/js_countries/countryClass.js
+++ public/js_countries/countryClass.js
@@ -30,7 +30,7 @@ export default class CountryClass{
   }
 
   async delCountry(){
-    let url = "http://localhost:3001/countries/"+this.id;
+    let url = "http://https://try-pmwa.onrender.com:3001/countries/"+this.id;
     try{
 
       let resp =  await axios({
--- public/js_countries/formEvent.js
+++ public/js_countries/formEvent.js
@@ -17,7 +17,7 @@ export const decalreFormEvent = (_doApi) => {
 
 
 const addNewCountry = async(_bodyData,_doApi) => {
-  let myUrl = "http://localhost:3001/countries"
+  let myUrl = "http://https://try-pmwa.onrender.com:3001/countries"
   try{
     let resp = await axios({
       url:myUrl,
